af0805670e56608497b2093eff4a229804d65a6c
Grigori Py <pygrigori@gmail.com>
2018-10-10 19:59:42 +0300

    molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -6,9 +7,36 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize('package', [
+    'ferm',
+    'patch'
+])
+def test_pkg_installed(host, package):
+    package = host.package(package)
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert package.is_installed
+
+
+@pytest.mark.parametrize('service', [
+    'ferm'
+])
+def test_service_is_enabled(host, service):
+    service = host.service(service)
+
+    assert service.is_enabled
+
+
+def test_config_exists(host):
+    file = host.file('/etc/ferm/ferm.conf')
+
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
+
+    print(file.content)
+
+
+def test_config_is_valid(host):
+    cmd = host.run('ferm --noexec /etc/ferm/ferm.conf')
+
+    assert not cmd.rc
